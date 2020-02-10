a23af4a5840b1d45a655e42c5e298b84fe475f79
Chris Cyr <chris.dana.cyr@gmail.com>
2020-02-10 11:51:08 -0600

    created Contact and Contact app and getters/setters

--- src/Contact.java
+++ src/Contact.java
@@ -1,2 +1,38 @@
+import java.awt.*;
+
 public class Contact {
+    private String name;
+    private String number;
+    private String email;
+
+    public Contact (String inputName, String inputNumber, String inputEmail) {
+        this.name = inputName;
+        this.number = inputNumber;
+        this.email = inputEmail;
+    }
+
+    public void setName(String inputName) {
+        this.name = inputName;
+    }
+
+    public void setNumber(String inputNumber) {
+        this.number = inputNumber;
+    }
+
+    public void setEmail(String inputEmail) {
+        this.email = inputEmail;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getNumber() {
+        return this.number;
+    }
+
 }
---
+++ src/ContactApp.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+
+public class ContactApp {
+    public static void main(String[] args) {
+
+    }
+}
